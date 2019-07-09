a4538a2b375d4ef9a6bb98ebed657d715be59a47
Charles Severance <csev@umich.edu>
2019-07-09 15:28:39 -0700

    Change method signature

--- django_tsugi/views.py
+++ django_tsugi/views.py
@@ -18,30 +18,30 @@ TSUGI_ERROR_URL = "https://www.tsugi.org/djangoerror"
 
 class LaunchView(View) :
 
-    def djangoerror(self, error, detail=false) :
+    def launcherror(self, error, detail=false) :
         url = TSUGI_ERROR_URL + '?detail=' + urllib.parse.urlencode({'detail': error})
         retval = HttpResponseRedirect(url)
         if ( detail ) retval['X-Tsugi-Detail'] = detail;
         return redirect(url)
 
     def get(self, request, success_url) :
-        return self.djangoerror('This is a Launch URL, expecting a POST with a JWT to initiate a launch')
+        return self.launcherror('This is a Launch URL, expecting a POST with a JWT to initiate a launch')
 
 
     def post(self, request, success_url) :
         encoded = request.POST.get('JWT')
         if encoded is None:
-            return self.djangoerror('This URL is expecting a POST with a JWT to initiate a launch')
+            return self.launcherror('This URL is expecting a POST with a JWT to initiate a launch')
 
         try:
             header = jwt.get_unverified_header(encoded)
         except:
-            return self.djangoerror('Could not load header from JWT',encoded)
+            return self.launcherror('Could not load header from JWT',encoded)
 
         try:
             kid = header.get('kid')
         except:
-            return self.djangoerror('The JWT is missing a key ID (kid)', encoded)
+            return self.launcherror('The JWT is missing a key ID (kid)', encoded)
 
         # Check if we already have the kid
         public_key = TSUGI_JWK_LIST.get(kid, False)
@@ -60,7 +60,7 @@ class LaunchView(View) :
                 dat = ""
 
             if len(dat) < 1 :
-                return self.djangoerror('Could not load keyset from '+keyset_url)
+                return self.launcherror('Could not load keyset from '+keyset_url)
 
             # https://github.com/latchset/jwcrypto/blob/master/jwcrypto/tests.py
             # https://jwcrypto.readthedocs.io/en/latest/jwk.html#classes
@@ -68,14 +68,14 @@ class LaunchView(View) :
                 ks = jwk.JWKSet()
                 ks.import_keyset(dat)
             except:
-                return self.djangoerror('Could not parse keyset from '+keyset_url,encoded)
+                return self.launcherror('Could not parse keyset from '+keyset_url,encoded)
 
 
             try:
                 k1 = ks.get_key(kid)
                 public_key = jwk.JWK.export_to_pem(k1)
             except:
-                return self.djangoerror('Could not extract kid='+kid+' from '+keyset_url,encoded)
+                return self.launcherror('Could not extract kid='+kid+' from '+keyset_url,encoded)
 
             if len(TSUGI_JWK_LIST) > 10 :
                 TSUGI_JWK_LIST.clear()
@@ -87,7 +87,7 @@ class LaunchView(View) :
             lti_launch = jwt.decode(encoded, public_key, algorithms=['RS256'])
             print(lti_launch)
         except:
-            return self.djangoerror('Could not validate JSON Web Token signature',encoded)
+            return self.launcherror('Could not validate JSON Web Token signature',encoded)
 
         request.session['lti_launch'] = lti_launch
         return redirect(reverse_lazy(success_url))
